a981e488181be5a55449dbdf49b8c41ec899791c
Lutfi <lutfilawliet2.0@gmail.com>
2024-12-21 23:24:18 +0700

    ubah

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,6 @@
   var year = new Date().getFullYear();
   document.querySelector("#currentYear").innerHTML = year;
 })();
+
+
+// aKg2GNHf6CXUZA4qbpHqbkP2qKEwFI2TY8/WQ6duxG7vL7LlJitjtfzOeygbeLIvRjphnuXugWZJnSOhttps://www.instagram.com/lutfiwliet/(ARMIN)YfX9b8hIiEnXXtQQWh3yQplh+lE8EJ+Enj2mkJZ4bhLHB3WNV
